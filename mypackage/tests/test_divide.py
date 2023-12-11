25ec0dd20cc3ca793cf4b90f0dc2df481c6170c4
Roland Axel Richter <roland.richter@empa.ch>
2023-12-11 10:34:58 +0100

    Added test_divide test file for pytest test

---
+++ mypackage/tests/test_divide.py
@@ -0,0 +1,14 @@
+#
+# Created on Mon Dec 11 2023
+#
+# Copyright (c) 2023 Your Company
+#
+
+"""_summary_
+"""
+
+def divide(a, b):
+    return a/b
+
+def test_divide():
+    assert divide(1, 2) == 0.5
